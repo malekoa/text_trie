0e85ed61b0a7ea02557ff94db77b3401a7050373
mazadegan <azadeganmalek@gmail.com>
2023-04-11 19:26:16 -0400

    added word sequence search

--- nary_tree.py
+++ nary_tree.py
@@ -1,3 +1,7 @@
+from typing import List
+
+from main import get_words
+
 class n_node:
     def __init__(self, val):
         self.val = val
@@ -14,6 +18,24 @@ class n_node:
 
     def set_val(self, val):
         self.val = val
+
+    # checks if a given sequence of words is in the trie starting at the current node
+    def contains_words(self, words: List[str]) -> bool:
+        # if there are no more words to check, return True
+        if len(words) == 0:
+            return True
+        # if there are more words to check, check if the current node has a child with the first word as the value
+        for child in self.children:
+            if child.val == words[0]:
+                # if the current node has a child with the first word as the value, recursively check if the child contains the rest of the words
+                return child.contains(words[1:])
+        # if the current node does not have a child with the first word as the value, return False
+        return False
+
+    # checks if a given sentence is in the trie starting at the current node
+    def contains_sentence(self, sentence: str) -> bool:
+        # check if the current node contains the words
+        return self.contains_words(get_words(sentence))
 
     def __str__(self):
         return str(f'[{self.val} {", ".join([str(child) for child in self.children])}]')
